40cfb59069a94fbba12d459e9db4eabd5c7ddb7b
Noah <E1528170@u.nus.edu>
2026-08-19 10:47:57 +0800

    Changed name

--- src/main/java/Furina.java
+++ src/main/java/Furina.java
@@ -3,9 +3,15 @@
  */
 public class Furina {
     public static void main(String[] args) {
-        String banner = "+------------------+\n"
-                + "|    F U R I N A   |\n"
-                + "+------------------+\n";
+        String separator = "____________________________________________________________";
+        String banner = "    F U R I N A";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Furina.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
