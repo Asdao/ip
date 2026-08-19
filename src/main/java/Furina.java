bd25f140232a244249ef295d7b45a3fb42891489
Noah <E1528170@u.nus.edu>
2026-08-19 10:54:08 +0800

    added echo

--- src/main/java/Furina.java
+++ src/main/java/Furina.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Furina chatbot.
  */
-public class Furina {
-    public static void main(String[] args) {
+class Furina {
+    static void main(String[] args) {
         String separator = "____________________________________________________________";
         String banner = "    F U R I N A";
 
@@ -11,7 +13,22 @@ public class Furina {
         System.out.println("Hello! I'm Furina.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                break;
+            }
+
+            System.out.println(separator);
+            System.out.println("    " + command);
+            System.out.println(separator);
+        }
+
+        System.out.println(separator);
+        System.out.println("    Bye. Hope to see you again soon!");
         System.out.println(separator);
     }
 }
