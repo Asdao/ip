fc1f85b515bbaaa738ddf44eb35341e643897847
Noah <E1528170@u.nus.edu>
2026-08-19 10:45:36 +0800

    Changed name

---
+++ src/main/java/Furina.java
@@ -0,0 +1,11 @@
+/**
+ * Entry point for the Furina chatbot.
+ */
+public class Furina {
+    public static void main(String[] args) {
+        String banner = "+------------------+\n"
+                + "|    F U R I N A   |\n"
+                + "+------------------+\n";
+        System.out.println(banner);
+    }
+}
